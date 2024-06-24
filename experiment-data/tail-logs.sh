0bd8d81ece355b739d37b17d552a63be20af922c
Alex <alexbrucely@gmail.com>
2024-06-24 09:22:51 -0700

    name

--- experiment-data/tail-logs.sh
+++ experiment-data/tail-logs.sh
@@ -5,7 +5,7 @@ start_namespace=1
 end_namespace=50
 
 # Define the output file
-output_file="450rps-10m-50-app-linkerd-l4auth-run-data-run-1.md"
+output_file="450rps-10m-50-app-1.22-istio-ambient-l4auth-run-data-run-1.md"
 
 # Define label selector for pods
 label_selector="app=vegeta"  # Modify this label selector as per your requirement
